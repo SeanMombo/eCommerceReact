b99b9046a967ef44a65273345dabfb0710f63246
SeanMombo <Sean@mombourquette.com>
2020-01-10 18:11:06 -0500

    added shopping cart and toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {Link, Switch, Route} from 'react-router-dom';
+
+import {Switch, Route, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,8 +10,8 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends Component {
@@ -49,15 +51,32 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
+          <Route 
+            exact
+            path='/signin' 
+            render={() => 
+            this.props.currentUser ? 
+              (<Redirect to='/' />)
+              : 
+              (<SignInAndSignUp />)
+            }
+          />
+            
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+  )(App);
