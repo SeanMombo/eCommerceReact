ce78d382d96c814fdb2df8274839449031c18f18
SeanMombo <Sean@mombourquette.com>
2020-01-12 13:09:19 -0500

    added selector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 const Header = ({currentUser, hidden}) => (
     <div className='header'>
