968ca5aa029dc46b016f98959514e43bcdf6e43f
SeanMombo <Sean@mombourquette.com>
2020-04-30 21:49:57 -0400

    started react saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger';
 import rootReducer from './root-reducer'
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { fetchCollectsStart } from './shop/shop.sagas'
 
-const middlewares = [thunk];
+const sagaMiddleWare = createSagaMiddleware();
+
+const middlewares = [sagaMiddleWare];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+sagaMiddleWare.run(fetchCollectsStart)
+
 export const persistor = persistStore(store)
 
 export default { store, persistor };
