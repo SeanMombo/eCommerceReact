5b392314915ac828e5b946accbd502973096edab
SeanMombo <Sean@mombourquette.com>
2020-05-06 15:21:18 -0400

    refactoring reused code into a new function

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,11 +5,9 @@ import { auth, googleProvider, createUserProfileDocument } from '../../firebase/
 
 import { signInSuccess, signInFailure} from './user.actions';
 
-
-export function* signInWithGoogle() {
+export function* getSnapshotFromUserAuth(userAuth) {
     try {
-        const {user} = yield auth.signInWithPopup(googleProvider);
-        const userRef = yield call(createUserProfileDocument, user);
+        const userRef = yield call(createUserProfileDocument, userAuth);
         const userSnapshot = yield userRef.get();
         yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
         );
@@ -18,13 +16,19 @@ export function* signInWithGoogle() {
     }
 }
 
+export function* signInWithGoogle() {
+    try {
+        const {user} = yield auth.signInWithPopup(googleProvider);
+        yield getSnapshotFromUserAuth(user);
+    } catch(error) {
+        yield put (signInFailure(error));
+    }
+}
+
 export function* signInWithEmail({payload: { email, password }}) {
     try {
         const { user } = yield auth.signInWithEmailAndPassword(email, password);
-        const userRef = yield call(createUserProfileDocument, user);
-        const userSnapshot = yield userRef.get();
-        yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
-        );
+        yield getSnapshotFromUserAuth(user);
     } catch(error) {
         put(signInFailure(error));
     }
