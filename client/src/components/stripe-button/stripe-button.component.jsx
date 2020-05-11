e0008ecdfa66d9b44b021210e30f4687141c3512
SeanMombo <Sean@mombourquette.com>
2020-05-11 12:55:32 -0400

    seperating our cra app into client folder

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_96HfILExyRt3Ir5neTw63OJH004fSixOIK';
+
+  const onToken = token => {
+    axios({
+      url: 'payment', 
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment Succesful')
+      })
+      .catch(error => { 
+        console.log('Payment error: ', error.response);
+        alert('There was an issue with your payment. Please make sure you use the provided credit card');
+    })
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+//import stripe library after requre('dotenv');
+//we do this bc we're importing the secret key from .env
+
+//returns stripe object that will allow us to make charges
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html')) 
+    })
+}
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('Server Running on port ' + port);
+})
+
+//req holds info of request — includes all the info we're sending with the request
+//res holds info of response 
+app.post('/payment', (req, res) => {
+    
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    };
+    
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({ error: stripeErr })
+        } else {
+            res.status(200).send({ success: stripeRes });
+        }
+    })
+})
