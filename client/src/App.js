b6b05eef3508d589c974672dd824c1324427f498
SeanMombo <Sean@mombourquette.com>
2020-05-15 19:39:18 -0400

    added media queries for responsive layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
